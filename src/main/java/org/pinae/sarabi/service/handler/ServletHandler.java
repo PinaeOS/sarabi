54f1f5c03648fa8337ce8e60ef0a0d5f82367022
Yugeng Hui <interhuiyg@163.com>
2017-04-14 21:01:54 +0800

    Update ServletHandler

--- src/main/java/org/pinae/sarabi/service/handler/ServletHandler.java
+++ src/main/java/org/pinae/sarabi/service/handler/ServletHandler.java
@@ -7,9 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.pinae.sarabi.service.ServiceConfig;
+import org.pinae.sarabi.service.ServiceContainer;
+
 public class ServletHandler extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private ServiceContainer container;
+	
+	public ServletHandler(ServiceContainer container) {
+		this.container = container;
+	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
              throws ServletException, IOException {
@@ -38,6 +47,12 @@ public class ServletHandler extends HttpServlet {
 	
 	private void execute(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-		String method = request.getMethod(); 
+		String uri = request.getRequestURI();
+		String method = request.getMethod();
+		
+		ServiceConfig srvCfg = this.container.getService(uri, method);
+		if (srvCfg != null) {
+			
+		}
 	}
 }
