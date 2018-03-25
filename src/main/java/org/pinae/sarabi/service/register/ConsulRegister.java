46a33c437e57fe5616c1e2c44ca3e544cc6f8917
huiyugeng <interhuiyg@163.com>
2018-03-25 23:22:06 +0800

    Add register stub class

---
+++ src/main/java/org/pinae/sarabi/service/register/ConsulRegister.java
@@ -0,0 +1,11 @@
+package org.pinae.sarabi.service.register;
+
+import org.pinae.sarabi.service.listener.RegisterListener;
+
+public class ConsulRegister implements RegisterListener {
+
+	public void register(boolean isRegister, String name, String description, String url, String[] method) {
+		
+	}
+
+}
---
+++ src/main/java/org/pinae/sarabi/service/register/ZazuRegister.java
@@ -0,0 +1,12 @@
+package org.pinae.sarabi.service.register;
+
+import org.pinae.sarabi.service.listener.RegisterListener;
+
+public class ZazuRegister implements RegisterListener {
+
+	public void register(boolean isRegister, String name, String description, String url, String[] method) {
+		
+
+	}
+
+}
